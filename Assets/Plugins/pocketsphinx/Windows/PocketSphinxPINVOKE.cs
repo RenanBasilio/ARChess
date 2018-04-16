9a7b1e28d221477bc8679a0bb17b1999ade1144a
Renan Basilio <renanbasilio@poli.ufrj.br>
2018-04-16 16:31:37 -0300

    Add pocketsphinx dlls for Windows build
    Add PocketSphinxPINVOKE interface script.
    Add VoiceRecognizer script.

---
+++ Assets/Plugins/pocketsphinx/Windows/PocketSphinxPINVOKE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public static class PocketSphinxPINVOKE {
+
+	[DllImport("libpocketsphinxwrap", CallingConvention = CallingConvention.Cdecl)]
+	public static extern IntPtr ps_args();
+
+	[DllImport("libpocketsphinxwrap", CallingConvention = CallingConvention.Cdecl)]
+	public static extern IntPtr ps_init(IntPtr args_l);
+}
---
+++ Assets/Scripts/VoiceRecognizer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System;
+using System.Runtime.InteropServices;
+
+public class VoiceRecognizer : MonoBehaviour {
+
+	#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+		IntPtr decoder;
+
+	#elif UNITY_ANDROID
+		AndroidJavaObject decoder;
+
+	#endif
+
+	AudioSource source_mic;
+
+	Text debugLog;
+
+	// Use this for initialization
+	void Start () {
+		debugLog = GameObject.Find("DebugLog").GetComponent<Text>();
+		debugLog.text = "Debug log found.\n";
+
+		debugLog.text += Application.streamingAssetsPath + "/VoiceModel/cmusphinx-en-us-ptm-8khz-5.2" + "\n";
+		debugLog.text += Directory.Exists(Application.streamingAssetsPath + "/VoiceModel/cmusphinx-en-us-ptm-8khz-5.2") + "\n";
+		SetupDecoder();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Set up a pocketsphinx decoder
+	void SetupDecoder () {
+		Debug.Log("Inicializando decodificador.");
+
+		#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+			Debug.Log(PocketSphinxPINVOKE.ps_args());
+			//decoder = PocketSphinxPINVOKE.ps_init(PocketSphinxPINVOKE.ps_args());
+			debugLog.text += decoder + "\n";
+		#elif UNITY_ANDROID
+			// Inicializa classe Decoder em java no pacote edu.cmu.pocketsphinx
+			AndroidJavaClass decoderClass = new AndroidJavaClass("edu.cmu.pocketsphinx.Decoder");
+
+			// Inicializa objeto config em java, que contem o resultado da chamada do metodo DefaultConfig da classe Decoder
+			AndroidJavaObject config = decoderClass.Call<AndroidJavaObject>("DefaultConfig");
+
+			// Constroi um array de parametros.
+			object[] hmm = new object[2] {"-hmm", Application.streamingAssetsPath + "/VoiceModel/cmusphinx-en-us-ptm-8khz-5.2"};
+
+			// Chama o metodo de java que configura os parametros
+			config.Call("SetString", hmm);
+		#endif
+	}
+}
