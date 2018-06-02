37613d050d1b36fc4e001d4f66d821d4f4f569e5
Lucas Barcellos Oliveira <lucas.barcellos.oliveira@poli.ufrj.br>
2018-06-01 23:10:32 -0300

    Checks if it's a pawn's first move. (Usage of lists.)

--- PrimeiraRA/Assets/colisao.cs
+++ PrimeiraRA/Assets/colisao.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class colisao : MonoBehaviour {
 	
 	GameObject selectedPiece;
-	GameObject[] newpos;
+	List<GameObject> newpos=new List<GameObject>();
 	
 	int[] numSquare(GameObject piece){
 		int[] ret={(int)Mathf.Round(Mathf.Abs(piece.transform.position.x-14)/4+1),(int)Mathf.Round((float)((piece.transform.position.y-2.32)/0.26+1))};
@@ -15,59 +15,63 @@ public class colisao : MonoBehaviour {
     void OnTriggerEnter (Collider col){
 		if (!selectedPiece){
 			selectedPiece=col.gameObject;
+			GameObject cube;
 			switch (selectedPiece.name.Split()[1]){
 				case "Pawn(Clone)":
-					newpos=new GameObject[2];
-					for (int i=0;i<newpos.Length;i++){
-						newpos[i]=GameObject.CreatePrimitive(PrimitiveType.Cube);
-						newpos[i].transform.rotation=selectedPiece.transform.rotation;
-						newpos[i].transform.position=selectedPiece.transform.position;
-						newpos[i].transform.Translate(4*(i+1),0,0); //forward
+					int moveForward=1;
+					if ((selectedPiece.name.Split()[0]=="White"&&numSquare(selectedPiece)[0]==2)||(selectedPiece.name.Split()[0]=="Black"&&numSquare(selectedPiece)[0]==7))
+						moveForward++;
+					for (int i=0;i<moveForward;i++){
+						cube=GameObject.CreatePrimitive(PrimitiveType.Cube);
+						cube.transform.rotation=selectedPiece.transform.rotation;
+						cube.transform.position=selectedPiece.transform.position;
+						cube.transform.Translate(4*(i+1),0,0); //forward
+						newpos.Add(cube);
 					}
 					break;
 				case "Bishop(Clone)":
-					newpos=new GameObject[1];
-					for (int i=0;i<newpos.Length;i++){
-						newpos[i]=GameObject.CreatePrimitive(PrimitiveType.Cube);
-						newpos[i].transform.rotation=selectedPiece.transform.rotation;
-						newpos[i].transform.position=selectedPiece.transform.position;
-						newpos[i].transform.Translate(4*(i+1),4*(i+1),0); //diagonally to the right
+					for (int i=0;i<2;i++){
+						cube=GameObject.CreatePrimitive(PrimitiveType.Cube);
+						cube.transform.rotation=selectedPiece.transform.rotation;
+						cube.transform.position=selectedPiece.transform.position;
+						cube.transform.Translate(4*(i+1),4*(i+1),0); //diagonally to the right
+						newpos.Add(cube);
 					}
 					break;
 				case "King(Clone)":
-					newpos=new GameObject[1];
-					for (int i=0;i<newpos.Length;i++){
-						newpos[i]=GameObject.CreatePrimitive(PrimitiveType.Cube);
-						newpos[i].transform.rotation=selectedPiece.transform.rotation;
-						newpos[i].transform.position=selectedPiece.transform.position;
-						newpos[i].transform.Translate(4*(i+1),-4*(i+1),0); //diagonally to the left
+					for (int i=0;i<1;i++){
+						cube=GameObject.CreatePrimitive(PrimitiveType.Cube);
+						cube.transform.rotation=selectedPiece.transform.rotation;
+						cube.transform.position=selectedPiece.transform.position;
+						cube.transform.Translate(4*(i+1),-4*(i+1),0); //diagonally to the left
+						newpos.Add(cube);
 					}
 					break;
 				case "Queen(Clone)":
-					newpos=new GameObject[3];
-					for (int i=0;i<newpos.Length;i++){
-						newpos[i]=GameObject.CreatePrimitive(PrimitiveType.Cube);
-						newpos[i].transform.rotation=selectedPiece.transform.rotation;
-						newpos[i].transform.position=selectedPiece.transform.position;
-						newpos[i].transform.Translate(4*(i+1),0,0); //diagonally to the left
+					for (int i=0;i<3;i++){
+						cube=GameObject.CreatePrimitive(PrimitiveType.Cube);
+						cube.transform.rotation=selectedPiece.transform.rotation;
+						cube.transform.position=selectedPiece.transform.position;
+						cube.transform.Translate(4*(i+1),0,0); //diagonally to the left
+						newpos.Add(cube);
 					}
 					break;
 				case "Knight(Clone)":
-					newpos=new GameObject[1];
-					for (int i=0;i<newpos.Length;i++){
-						newpos[i].transform.rotation=selectedPiece.transform.rotation;
-						newpos[i].transform.position=selectedPiece.transform.position;
-						newpos[i]=GameObject.CreatePrimitive(PrimitiveType.Cube);
-						newpos[i].transform.Translate(8,4,0);
+					for (int i=0;i<1;i++){
+						cube=GameObject.CreatePrimitive(PrimitiveType.Cube);
+						cube.transform.rotation=selectedPiece.transform.rotation;
+						cube.transform.position=selectedPiece.transform.position;
+						cube.transform.Translate(8,4,0);
+						newpos.Add(cube);
 					}
 					break;
 				case "Rook(Clone)":
-					newpos=new GameObject[1];
-					for (int i=0;i<newpos.Length;i++){
-						newpos[i]=GameObject.CreatePrimitive(PrimitiveType.Cube);
-						newpos[i].transform.rotation=selectedPiece.transform.rotation;
-						newpos[i].transform.position=selectedPiece.transform.position;
-						newpos[i].transform.Translate(0,-4*(i+1),0);
+					for (int i=0;i<1;i++){
+						cube=GameObject.CreatePrimitive(PrimitiveType.Cube);
+						cube.transform.rotation=selectedPiece.transform.rotation;
+						cube.transform.position=selectedPiece.transform.position;
+						cube.transform.Translate(0,-4*(i+1),0);
+						newpos.Add(cube);
 					}
 					break;
 			}
@@ -78,8 +82,9 @@ public class colisao : MonoBehaviour {
 				selectedPiece.transform.parent=null;
 				selectedPiece.transform.rotation=col.gameObject.transform.rotation;
 				selectedPiece.transform.position=col.gameObject.transform.position;
-				for (int i=0;i<newpos.Length;i++)
+				for (int i=0;i<newpos.Count;i++)
 					Destroy(newpos[i]);
+				newpos.Clear();
 				selectedPiece=null;
 			}
 		}
