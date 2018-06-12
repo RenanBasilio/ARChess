c9df59c0b607d89c92d8ff925442730fd124fc79
Lucas Barcellos Oliveira <lucas.barcellos.oliveira@poli.ufrj.br>
2018-06-12 02:03:30 -0300

    Adds piece capture. Reorganizes files. Improves bishops' code.

---
+++ PrimeiraRA/Assets/Scripts/pecas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pecas : MonoBehaviour {
+	
+	int rodadaUltimoMovimento=0;
+	
+	public int getRodadaUltimoMovimento(){
+		return rodadaUltimoMovimento;
+	}
+	
+	public void setRodadaUltimoMovimento(int rodada){
+		rodadaUltimoMovimento=rodada;
+	}
+	
+	void OnTriggerStay(Collider col){
+		pecas script=col.gameObject.GetComponent<pecas>();
+		if (gameObject.transform.parent==null && rodadaUltimoMovimento>script.getRodadaUltimoMovimento() && gameObject.name.Split()[0]!=col.gameObject.name.Split()[0])
+			Destroy(col.gameObject);
+	}
+}
