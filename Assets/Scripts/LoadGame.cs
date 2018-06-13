167ea9609b6aa7fcd9811095dc4f1f77998ed626
Renan Basilio <renanbasilio@poli.ufrj.br>
2018-06-12 22:47:58 -0300

    Adiciona tela de título
    Adiciona assets de xadrez.
    Adiciona script que imprime OK quando o marcador é detectado.

---
+++ Assets/Scripts/LoadGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class LoadGame : MonoBehaviour, ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+
+	// Use this for initialization
+	void Start () {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ITrackableEventHandler.OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            Debug.Log("OK");
+        }
+    }
+}
